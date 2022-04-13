66c1eef678a6a4a06ed22e22f90d5fd146e225e5
Gabriele Frattini <gabriele.g.frattini@gmail.com>
2022-04-13 19:39:40 +0200

    websocket

--- api/routing.py
+++ api/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'wss/chat', consumers.VideoConsumer.as_asgi())
+    re_path(r'ws/chat', consumers.VideoConsumer.as_asgi())
 ]
 
