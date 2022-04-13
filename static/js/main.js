99b7aaac851df556f4ca082f5f6f5c34e3405976
Gabriele Frattini <gabriele.g.frattini@gmail.com>
2022-04-13 20:58:02 +0200

    ws

--- static/js/main.js
+++ static/js/main.js
@@ -27,7 +27,7 @@ const localVideo = document.querySelector('#user-1');
 
 // var endPoint = 'wss://'+window.location.host+'/ws/chat'
 
-var endPoint = 'wss://django-chat-app.herokuapp.com/wss/chat'
+var endPoint = 'wss://videoapp-rtc.herokuapp.com/wss/chat'
 
 
 var btnJoin = document.querySelector('#btn-join');
